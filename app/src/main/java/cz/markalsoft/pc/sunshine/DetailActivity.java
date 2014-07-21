49587aa64dd1e7985e8e7d7dc47714caa355c9c5
Martin Kalenda <chaosikcz@gmail.com>
2014-07-21 19:40:00 +0200

    added share action provider into detail activity

--- app/src/main/java/cz/markalsoft/pc/sunshine/DetailActivity.java
+++ app/src/main/java/cz/markalsoft/pc/sunshine/DetailActivity.java
@@ -3,15 +3,21 @@ package cz.markalsoft.pc.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,7 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+
         return true;
     }
 
@@ -44,23 +51,54 @@ public class DetailActivity extends ActionBarActivity {
             Intent intent = new Intent(DetailActivity.this,SettingsActivity.class);
             startActivity(intent);
         }
+
         return super.onOptionsItemSelected(item);
     }
-
+    // -------FRAGMENT---------------
     public static class PlaceholderFragment extends Fragment {
 
+        ShareActionProvider mShareActionProvider;
+        private static final String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+        private String mForecastStr;
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(doShare());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+
+        }
+
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            String weatherData = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_textview);
-            textView.setText(weatherData);
+            textView.setText(mForecastStr);
 
             return rootView;
+        }
+
+        private Intent doShare() {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            TextView textView = (TextView) getActivity().findViewById(R.id.detail_textview);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORECAST_SHARE_HASHTAG);
+            return intent;
         }
     }
 }
